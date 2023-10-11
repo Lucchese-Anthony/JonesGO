aaf41cd6c5923b0bb9a9f2dff96a8a1d384542bc
codehere21 <alena.litouka@gmail.com>
2023-10-11 18:55:37 -0400

    first commit

---
+++ backend/jonesgo/src/main/java/enigmatic/juggernauts/adv/innov3/jonesgo/JonesgoApplication.java
@@ -0,0 +1,13 @@
+package enigmatic.juggernauts.adv.innov3.jonesgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonesgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonesgoApplication.class, args);
+	}
+
+}
---
+++ backend/jonesgo/src/test/java/enigmatic/juggernauts/adv/innov3/jonesgo/JonesgoApplicationTests.java
@@ -0,0 +1,13 @@
+package enigmatic.juggernauts.adv.innov3.jonesgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonesgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
